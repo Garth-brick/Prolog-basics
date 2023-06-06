a10337112328ca0fa46c55635f34d1dc45bc9732
Vagarth <imvagarth@gmail.com>
2023-06-07 00:21:06 +0530

    first web scraping

--- web_scraping.pl
+++ web_scraping.pl
@@ -1,3 +1,10 @@
-:- use_module(library(http)).
+:- use_module(library(http/http_open)).
 :- use_module(library(lists)).
 
+% the top-level is enough to do web-scraping
+
+% ?- http_open("https://news.ycombinator.com", S, []),
+% load_html(stream(S), DOM, []).
+
+% this doesn't get the article titles I guess
+% xpath(DOM, //a(@class="storylink", text), E).
